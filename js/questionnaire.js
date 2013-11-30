655fde7428af92a12cfa5dfcb838952525c72795
naoki kodama <naoki.koda@gmail.com>
2013-11-30 14:40:26 +0900

    フォームからの取得を追加した

--- js/questionnaire.js
+++ js/questionnaire.js
@@ -1,12 +1,19 @@
 $(function() {
 
   // サブミットされたら、下の画面に表示する
-  $(XX).on("submit",function(){
+  $("#questionnaire").on("submit",function(){
     var name = $('#name').val();
-    var keep = $('#keep').val();
-    var problem = $('#problem').val();
-    var try = $('#try').val();
+    var keep_text = $('#keep').val();
+    var problem_text = $('#problem').val();
+    var try_text = $('#try').val();
 
+    console.log(name);
+    console.log(keep_text);
+    console.log(problem_text);
+    console.log(try_text);
+
+    $("#questionnaire_show").prepend($('<p>').html(name));
+    return false;
   });
 });
 
