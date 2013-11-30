69a0a0890e4eeb3f7daa48f62f946ec89da0b095
naoki kodama <naoki.koda@gmail.com>
2013-11-30 14:09:31 +0900

    インデクスを追加

--- js/questionnaire.js
+++ js/questionnaire.js
@@ -1,5 +1,13 @@
 $(function() {
 
+  // サブミットされたら、下の画面に表示する
+  $(XX).on("submit",function(){
+    var name = $('#name').val();
+    var keep = $('#keep').val();
+    var problem = $('#problem').val();
+    var try = $('#try').val();
+
+  });
 });
 
 
